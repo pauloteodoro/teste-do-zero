7baa335577639ae9c3ebeb43ee6a4fede7e826ee
pauloteodoro <40544482+pauloteodoro@users.noreply.github.com>
2018-09-09 00:00:19 -0300

    inicio

---
+++ inicio.projeto.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <Windows.h>
+#include <stdlib.h>
+#include <string>
+
+#define limpa system ("cls")
+#define pausa system ("pause")
+
+int main () {
+	
+	// projeto do zero;
+	
+}
